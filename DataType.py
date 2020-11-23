3bd5cca5540bce7972fde66b5fc6ac600d8a9571
Nikunj Goenka <nk53359@Dal.ca>
2020-11-23 15:09:10 +0530

    Search and Create Table

---
+++ DataType.py
@@ -0,0 +1,15 @@
+ALL_DATA_TYPES = {
+    "str": ["char", "varchar", "varbinary", "tinyBlob", "text", "longText"],
+    "int": ["smallint", "int", "mediumint", "integer", "bigint"],
+    "float": ["float", "double", "decimal"]
+}
+
+
+def getPythonBasedDataType(value):
+    value = value.lower()
+    for datatype in ALL_DATA_TYPES:
+        for testType in ALL_DATA_TYPES[datatype]:
+            if (testType in value):
+                return datatype
+
+    return None

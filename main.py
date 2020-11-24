0e95bb942a35f732e57675dcde29f989d7078bd2
souvikdas <Souvik.Das@dal.ca>
2020-11-24 12:18:27 -0400

    Adding the condition for quiting the SQL flow.

--- main.py
+++ main.py
@@ -26,16 +26,19 @@ def userLoginSignUp():
 
 
 def main():
+
     print("\n#####################################################")
     print("     Welcome to Team-8 DataBase Management System      ")
     print("#####################################################\n")
     userLoginSignUp()
-    query = input(constants.InputQuery)
-    query_type = Parse.Parse(query)
-    val = query_type.check_query()
+    query = ""
+    while not query.lower() == "quit":
+        query = input(constants.InputQuery)
+        query_type = Parse.Parse(query)
+        val = query_type.check_query()
 
-    if val == -1:
-        print("Incorrect Query")
+        if val == -1:
+            print("Incorrect Query")
 
 
 if __name__ == "__main__":

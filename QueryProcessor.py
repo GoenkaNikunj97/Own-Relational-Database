aaa5262bfde26c83c083408e06d48509cf2eeb97
Nikunj Goenka <nk53359@Dal.ca>
2020-11-24 20:03:55 +0530

    create/use DB and Test added

--- QueryProcessor.py
+++ QueryProcessor.py
@@ -5,58 +5,74 @@ import json
 import DataType
 import Presentation
 
+class QueryProcessor:
 
-class QueryProcessos:
-
-    def __init__(self, databaseName):
-        self.databaseName = databaseName
-        self.databaseDir = "AllDatabase/" + self.databaseName + "/"
-
-    def checkIfFilePathExist(self, tableDir):
-        if os.path.exists(tableDir):
+    def __init__(self):
+        self.databaseName = ""
+        self.databaseDir = ""
+    
+    def checkIfFilePathExist(self, dirName):
+        if os.path.exists(dirName):
             return True
         else:
             return False
-
-    def createTable(self, tableName, columnDict, primaryKeyList, foreignKeyList):
-
-        tableDir = self.databaseDir + tableName + "/"
-        tableMetadatFilePath = tableDir + tableName + "_metadata.json"
-        tableDataFilePath = tableDir + tableName + "_data.json"
-
-        if self.checkIfFilePathExist(tableDir):
-            raise Exception("table already Exist")
+        
+    def useDb(self, DBName):
+        dbDir = "AllDatabase/" + DBName
+        if( self.checkIfFilePathExist(dbDir)):
+            self.databaseName = DBName
+            self.databaseDir = "AllDatabase/" + self.databaseName + "/"
+            print(self.databaseName, " Selected")
         else:
-            os.makedirs(tableDir)
+            raise Exception("Database " +DBName+" Does Not Exist")
+        
+    def createDB(self, DBName):
+        dbDir = "AllDatabase/" + DBName
+        if( self.checkIfFilePathExist(dbDir)):
+            raise Exception("Database already Exist")
+        else:
+            os.makedirs(dbDir)
+            print(DBName, " Created")
+            self.useDb(DBName)
 
-        tabelMatadata = dict()
-        tabelMatadata["table_name"] = tableName
-        columnList = list()
+    def createTable(self, tableName, columnDict, primaryKeyList, foreignKeyList=[]):
+        if(self.databaseName == ""):
+            raise Exception("Select Database first")
+        else:
+            tableDir = self.databaseDir + tableName + "/"
+            tableMetadatFilePath = tableDir + tableName + "_metadata.json"
+            tableDataFilePath = tableDir + tableName + "_data.json"
 
-        for key in columnDict.keys():
-            columnInputType = DataType.getPythonBasedDataType(columnDict[key])
-            columnList.append({"column_name": key, "type": columnInputType})
+            if self.checkIfFilePathExist(tableDir):
+                raise Exception("table already Exist")
+            else:
+                os.makedirs(tableDir)
 
-        tabelMatadata["columns"] = columnList
-        tabelMatadata["primary_key"] = primaryKeyList
-        tabelMatadata["foreign_key"] = foreignKeyList
-        tabelMatadata = json.dumps(tabelMatadata)
+            tabelMatadata = dict()
+            tabelMatadata["table_name"] = tableName
+            columnList = list()
 
-        with open(tableMetadatFilePath, 'w') as f:
-            f.truncate()
-            f.write(tabelMatadata)
+            for key in columnDict.keys():
+                columnInputType = DataType.getPythonBasedDataType(columnDict[key])
+                columnList.append({"column_name": key, "type": columnInputType})
 
-        with open(tableDataFilePath, 'w') as f:
-            f.truncate()
+            tabelMatadata["columns"] = columnList
+            tabelMatadata["primary_key"] = primaryKeyList
+            tabelMatadata["foreign_key"] = foreignKeyList
+            tabelMatadata = json.dumps(tabelMatadata)
 
-    def readTable(self, tableName, columnListToDisplay=[], condition={}):
-        '''
-            condition = {
-                "columnName" : "PersonID",
-                "operator" : "=",
-                "value" : "1"
-            }
-        '''
+            with open(tableMetadatFilePath, 'w') as f:
+                f.truncate()
+                f.write(tabelMatadata)
+
+            with open(tableDataFilePath, 'w') as f:
+                f.truncate()
+                f.write("[]")
+
+            print(tableName + " Table Created")
+
+    def selectQuery(self, tableName, columnListToDisplay=[], condition={}):
+        
         tableDir = self.databaseDir + tableName + "/"
         
         if self.checkIfFilePathExist(tableDir):
@@ -65,57 +81,58 @@ class QueryProcessos:
             
             with open (tableDataFilePath) as file:
                 tableData = json.load(file)
-            if( len(condition) > 0 ):
-                data = list()
-                for row in tableData :
-                    colToCheck = condition["columnName"]
-                    operator = condition["operator"]
-                    valueToCheck = condition["value"]
-                    valueType = type(valueToCheck)
-                    if( colToCheck in row.keys()):
-                        if (operator == "="):
-                            if(str(row[colToCheck]) == (valueToCheck)):
-                                data.append(row)
-                        elif (operator == ">"):
-                            if(str(row[colToCheck]) > (valueToCheck)):
-                                data.append(row)
-                        elif (operator == "<"):
-                            if(str(row[colToCheck]) < (valueToCheck)):
-                                data.append(row)
-                        elif (operator == ">="):
-                            if(str(row[colToCheck]) >= (valueToCheck)):
-                                data.append(row)
-                            
-                        elif (operator == "<="):
-                            if(str(row[colToCheck]) <= (valueToCheck)):
-                                data.append(row)
-                        elif (operator == "!="):
-                            if(str(row[colToCheck]) != (valueToCheck)):
-                                data.append(row)
+            if(len(tableData) == 0):
+                print(tableName + " Table is Empty")
+                return
+            else:
+                if( len(condition) > 0 ):
+                    data = list()
+                    for row in tableData :
+                        colToCheck = condition["columnName"]
+                        operator = condition["operator"]
+                        valueToCheck = condition["value"]
+                        valueType = type(valueToCheck)
+                        if( colToCheck in row.keys()):
+                            if (operator == "="):
+                                if(str(row[colToCheck]) == (valueToCheck)):
+                                    data.append(row)
+                            elif (operator == ">"):
+                                if(str(row[colToCheck]) > (valueToCheck)):
+                                    data.append(row)
+                            elif (operator == "<"):
+                                if(str(row[colToCheck]) < (valueToCheck)):
+                                    data.append(row)
+                            elif (operator == ">="):
+                                if(str(row[colToCheck]) >= (valueToCheck)):
+                                    data.append(row)
+                                
+                            elif (operator == "<="):
+                                if(str(row[colToCheck]) <= (valueToCheck)):
+                                    data.append(row)
+                            elif (operator == "!="):
+                                if(str(row[colToCheck]) != (valueToCheck)):
+                                    data.append(row)
+                    tableHeader = dict()
+                    for key in tableData[0].keys():
+                        tableHeader[key] = ""
+                    data.insert(0 , tableHeader)
+                    tableData = data
+                    
+                if(len(columnListToDisplay) > 0):
+                    data = list()
+                    for row in tableData :
+                        selectedCols = dict()
+                        for col in columnListToDisplay:
+                            selectedCols[col] = row[col]
+                        data.append(selectedCols)
+                    tableHeader = dict()
+                    for key in data[0].keys():
+                        tableHeader[key] = ""
+                    data.insert(0 , tableHeader)
+                    tableData = data
+  
+            Presentation.displayJSONListData(tableData)
+        else:
+            raise Exception(tableName + " Table does not Exist")
+            
 
-                tableData = data
-
-        Presentation.displayJSONListData(tableData)
-
-
-if __name__ == "__main__":
-
-    queryProcessor = QueryProcessos("PersonDataBase")
-    a = {
-        'PersonID': 'int',
-        'LastName': 'varchar(255)',
-        'FirstName': 'varchar(255)',
-        'Address': 'varchar(255)',
-        'City': 'varchar(255)'
-    }
-    try:
-        queryProcessor.createTable(
-            "SecondTable", a, ['PersonID', 'Address'], [])
-    except Exception as e:
-        print(e)
-    condition = {
-                "columnName" : "PersonID",
-                "operator" : "!=",
-                "value" : "3"
-            }
-    queryProcessor.readTable( "Person", condition = condition )
